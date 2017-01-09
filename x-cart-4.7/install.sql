4d98643c3955e65d88d624a2c533b367d8c15884
Tyson <tyson@vietnamcubator.com>
2017-01-09 16:00:42 +0700

    Update xcart plugin

---
+++ x-cart-4.7/install.sql
@@ -0,0 +1,5 @@
+INSERT INTO xcart_ccprocessors SET module_name='Targetpay-iDEAL', type='P', processor='cc_targetpay_ideal.php', template='cc_targetpay_ideal.tpl', disable_ccinfo='Y', background='N', testmode='N', param01='93929';
+
+INSERT INTO xcart_ccprocessors SET module_name='Targetpay-Mister-Cash', type='P', processor='cc_targetpay_mrcash.php', template='cc_targetpay_mrcash.tpl', disable_ccinfo='Y', background='N', testmode='N', param01='93929';
+
+INSERT INTO xcart_ccprocessors SET module_name='Targetpay-Sofort', type='P', processor='cc_targetpay_deb.php', template='cc_targetpay_deb.tpl', disable_ccinfo='Y', background='N', testmode='N', param01='93929';
