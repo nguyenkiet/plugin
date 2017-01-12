ae50e119eec95c5d57e562a378e16b550b1aa70b
Tyson <tyson@vietnamcubator.com>
2017-01-12 11:22:00 +0700

    Add latest code for xCart

---
+++ xcart/v5.3/classes/XLite/Module/TargetPay/Payment/Processor/Sofort.php
@@ -0,0 +1,59 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * @file         Provides support for TargetPay iDEAL, Mister Cash, Sofort Banking, Credit and Paysafe
+ * @author         Yellow Melon B.V.
+ * @url         http://www.idealplugins.nl
+ */
+
+namespace XLite\Module\TargetPay\Payment\Processor;
+
+use XLite\Module\TargetPay\Payment\Base\TargetPayPlugin;
+
+class Sofort extends TargetPayPlugin
+{
+    /**
+     * The contructor
+     */
+    public function __construct()
+    {
+        $this->payMethod = "DEB";
+        $this->appId       = "382a92214fcbe76a32e22a30e1e9dd9f";
+        $this->currency  = "EUR";
+        $this->language  = 'nl';
+        $this->allow_nobank = true;
+    }
+    
+    /**
+    * The setting widget
+    * {@inheritDoc}
+    * @see \XLite\Model\Payment\Base\Processor::getSettingsWidget()
+    */
+    public function getSettingsWidget()
+    {
+        return 'modules/TargetPay/Sofort/config.twig';
+    }
+    
+    /**
+     * return transaction process
+     * {@inheritDoc}
+     * @see \XLite\Model\Payment\Base\Online::processReturn()
+     */
+    public function processReturn(\XLite\Model\Payment\Transaction $transaction)
+    {
+        parent::processReturn($transaction);
+        $this->handlePaymentResult($transaction, false);
+    }
+    
+    /**
+     * Callback message
+     * {@inheritDoc}
+     * @see \XLite\Model\Payment\Base\Online::processCallback()
+     */
+    public function processCallback(\XLite\Model\Payment\Transaction $transaction)
+    {
+        parent::processCallback($transaction);
+        $this->handlePaymentResult($transaction, true);
+    }
+}
